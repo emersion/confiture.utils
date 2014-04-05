ef55a148f2a5bcef3af3543b0fc3ecb72d6e7dee
Simon Ser <contact@emersion.fr>
2014-04-05 12:04:37 +0200

    Updates lighp-core

--- massctl.sh
+++ massctl.sh
@@ -5,6 +5,8 @@
 APP_VERSION='0.1'
 APP_AUTHOR='$imon'
 
+collectChanges=false
+collectChangesNoConfirm=false
 generatePkgs=false
 addPkgsToRepo=false
 
@@ -16,9 +18,10 @@ do
 		echo "    written by $APP_AUTHOR           "
 		echo ""
 
-		echo "Usage : ./massctl.sh [--base-dir BASE_DIR] --repository REPO_PATH [--generate] [--add-to-repo]"
-		echo "Usage : ./massctl.sh [-b BASE_DIR] -r REPO_PATH [-G] [-A]"
+		echo "Usage : ./massctl.sh [--repository REPO_PATH] [--base-dir BASE_DIR] [--lighp-path LIGHP_PATH] [--collect-changes] [--collect-noconfirm] [--generate] [--add-to-repo]"
+		echo "Usage : ./massctl.sh [-r REPO_PATH] [-b BASE_DIR] [--l LIGHP_PATH] [-C] [-G] [-A]"
 		echo "BASE_DIR: by default, current directory"
+		echo "LIGHP_PATH: by default, none"
 		exit
 	fi
 
@@ -30,6 +33,19 @@ do
 	if [ "$1" = '--repository' ] || [ "$1" = '-r' ] ; then
 		shift
 		repoPath=$(readlink -f "$1")
+	fi
+
+	if [ "$1" = '--lighp-path' ] || [ "$1" = '-l' ] ; then
+		shift
+		lighpPath=$(readlink -f "$1")
+	fi
+
+	if [ "$1" = '--collect-changes' ] || [ "$1" = '-C' ] ; then
+		collectChanges=true
+	fi
+
+	if [ "$1" = '--collect-noconfirm' ] ; then
+		collectChangesNoConfirm=true
 	fi
 
 	if [ "$1" = '--generate' ] || [ "$1" = '-G' ] ; then
@@ -49,6 +65,39 @@ fi
 
 pkgFolders=( $(ls "$rootPath") )
 
+if [ $collectChanges = true ] ; then
+	if [ -z "$lighpPath" ] ; then
+		read -p "Lighp's path : " lighpPath
+	fi
+
+	if [ ! -d "$lighpPath" ] ; then
+		echo "Lighp dir \"$lighpPath\" doesn't exists."
+		exit
+	fi
+
+	for pkgName in ${pkgFolders[@]}
+	do
+		if [ ! -d "$pkgName" ] ; then
+			continue
+		fi
+
+		pkgDir="$rootPath/$pkgName"
+
+		if [ ! -d "$pkgDir/src" ] ; then
+			continue
+		fi
+
+		echo "Collecting changes for package in folder \"$pkgDir\"..."
+
+		args="--input \"$pkgDir\" --lighp-path \"$lighpPath\""
+		if [ $collectChangesNoConfirm = true ] ; then
+			args="$args --yes"
+		fi
+
+		eval "./changescollector.sh $args"
+	done
+fi
+
 if [ $generatePkgs = true ] ; then
 	for pkgName in ${pkgFolders[@]}
 	do
@@ -62,7 +111,7 @@ if [ $generatePkgs = true ] ; then
 			continue
 		fi
 
-		echo "Adding package in folder \"$pkgDir\"..."
+		echo "Generating package in folder \"$pkgDir\"..."
 		./generatepkg.sh --name "$pkgName" --input "$pkgDir" --output "$pkgDir"
 	done
 fi
